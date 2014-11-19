11d2e78c4544fbb8006b246720f8aa578e96349a
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-11-19 23:13:51 +0000

    Sorted out tests and module layout.
    A setup.py has been made, testing works from setup.py and events have been basically implemented.
    
    I have also included a .bat file for use with doxygen for those of us that use windows...

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ""
+
+setup(name='seprcph',
+      packages=find_packages(),
+      version='0.0.1',
+      description='A game about trains in europe.',
+      author='Harvey Hunt, Henry Jewell, Flinn Burgess, Peter Bryant, Alex Corney, Ethan La Bonche Vince-Urwin',
+      url='https://github.com/HarveyHunt/seprcph',
+      keywords="python pygame trains europe",
+      install_requires=[],
+      long_description=read('README.md'),
+      test_suite='tests',
+      entry_points={'console_scripts': ['seprcph=seprcph.main:main']})
---
+++ tests/event.py
@@ -0,0 +1,20 @@
+import unittest
+
+from seprcph import event
+
+class TestCreateEvent(unittest.TestCase):
+
+    def test_no_desc_event(self):
+        e = event.Event('topic', data={1: 2})
+        self.assertIsNone(e.desc)
+
+    def test_no_data_event(self):
+        e = event.Event('topic', desc='desc')
+        self.assertIsNone(e.data)
+
+    def test_correct_event(self):
+        e = event.Event('topic', 'desc', {1: 2})
+        self.assertIsNotNone(e)
+
+if __name__ == '__main__':
+    unittest.main()
