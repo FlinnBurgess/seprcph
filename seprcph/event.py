2c0d08bc62f318122ca14a9f8929eed6ba0b6d58
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-11-20 01:47:31 +0000

    Updated tests and prevented name mangling.

--- seprcph/event.py
+++ seprcph/event.py
@@ -6,31 +6,28 @@ class Event(object):
     Lightweight class that represents a generic event
     """
     def __init__(self, topic, desc=None, **kwargs):
-        """ 
+        """
         Constructor for Event class
 
         Args:
             topic: String denoting the topic that the event relates to
             desc: String, optional, for displaying the event textually
             **kwargs: Optional data arguments for event handlers to use
-
-        Raises:
-            SEVERAL QUESTIONS
         """
-        
+
         self.topic = topic
         self.desc = desc
         self.data = kwargs
 
     def __str__(self):
-        return self.desc
+        return "%s | %s : %s" (self.topic, self.desc, self.data)
 
 
 class EventManager(object):
     """
     Static class that manages the publishing and handling of messages
     """
-    __subscriptions = {}
+    _subscriptions = {}
 
     @staticmethod
     def add_listener(topic, callback):
@@ -41,13 +38,10 @@ class EventManager(object):
         Args:
             topic: The topic to which the listener will subscribe
             callback: The event handler method of the listening object
-
-        Raises:
-            SEVERAL QUESTIONS
         """
 
-        EventManager.__subscriptions.setdefault(topic, [])
-        EventManager.__subscriptions[topic].append(callback)
+        EventManager._subscriptions.setdefault(topic, [])
+        EventManager._subscriptions[topic].append(callback)
 
     @staticmethod
     def remove_listener(topic, callback):
@@ -57,12 +51,8 @@ class EventManager(object):
         Args:
             topic: The topic form which the listener will be unsubscribed
             callback: The event handler method of the listening object
-
-        Raises:
-            SEVERAL QUESTIONS
         """
-
-        EventManager.__subscriptions[topic].remove(callback)
+        EventManager._subscriptions[topic].remove(callback)
 
     @staticmethod
     def notify_listeners(event):
@@ -76,7 +66,7 @@ class EventManager(object):
             AssertionError
         """
 
-        assert(event.topic in EventManager.__subscriptions.keys())
+        assert(event.topic in EventManager._subscriptions.keys())
 
-        for handler in EventManager.__subscriptions[event.topic]:
+        for handler in EventManager._subscriptions[event.topic]:
             handler(event)
--- tests/event.py
+++ tests/event.py
@@ -4,14 +4,42 @@ from seprcph import event
 
 
 class TestCreateEvent(unittest.TestCase):
-    
+
     def test_no_data_event(self):
-        e = event.Event('topic', desc='desc')
-        self.assertIsNone(e.data)
+        e = event.Event('topic', 'desc')
+        self.assertEqual(e.data, {})
+
+    def test_no_desc_event(self):
+        e = event.Event('topic', foo=2)
+        self.assertIsNone(e.desc)
 
     def test_correct_event(self):
-        e = event.Event('topic', 'desc', {1: 2})
+        e = event.Event('topic', 'desc', foo=2)
         self.assertIsNotNone(e)
+
+class TestEventManager(unittest.TestCase):
+
+    def setUp(self):
+        self.x = 0
+        self.em = event.EventManager()
+
+    def _func(self, _):
+        self.x = 1
+
+    def test_attach_listener(self):
+        self.em.add_listener('foo', self._func)
+        self.assertEqual(len(self.em._subscriptions), 1)
+
+    def test_notify_unknown_listener(self):
+        self.assertRaises(AssertionError, self.em.notify_listeners, event.Event('bar'))
+
+    def test_notify_listeners(self):
+        self.em.notify_listeners(event.Event('foo'))
+        self.assertEqual(self.x, 1)
+
+    def test_remove_listener(self):
+        event.EventManager.remove_listener('foo', self._func)
+        self.assertEqual(len(event.EventManager._subscriptions), 0)
 
 if __name__ == '__main__':
     unittest.main()
