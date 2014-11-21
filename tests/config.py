10cd502dfeab17e46bb01b2810fa2f73dd5e2071
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-11-21 18:20:19 +0000

    PEP8 fixes, don't generate tex and added more unit tests.

--- tests/config.py
+++ tests/config.py
@@ -3,6 +3,7 @@ import os
 
 from seprcph import config
 
+
 class TestConfig(unittest.TestCase):
     path = os.path.join(os.path.expanduser('~'), 'seprcph', 'config')
 
@@ -26,6 +27,7 @@ class TestConfig(unittest.TestCase):
         with open(path, 'w') as f:
             f.write(config_string)
 
+
 class TestDataTypeReplacement(unittest.TestCase):
 
     def test_replace_true(self):
@@ -43,3 +45,11 @@ class TestDataTypeReplacement(unittest.TestCase):
 
     def test_replace_int(self):
         self.assertEqual(config.Config._replace_data_types({'': '1'})[''], 1)
+
+    def test_replace_list(self):
+        self.assertEqual(config.Config._replace_data_types({'': '1, 2, 3'})[''], ['1', '2', '3'])
+
+    def test_replace_log_level(self):
+        for level, value in {'NONE': 0, 'NULL': 0, 'DEBUG': 10, 'INFO': 20,
+                        'WARNING': 30, 'ERROR': 40, 'CRITICAL': 50}.items():
+            self.assertEqual(config.Config._replace_data_types({'': level})[''], value)
