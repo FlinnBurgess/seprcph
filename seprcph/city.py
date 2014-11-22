dcc73079cc3512d7d76b04bcacc0e5d197c23281
FlinnBurgess <flinn.burgess@gmail.com>
2014-11-22 17:21:57 +0000

    Began implementation of city class

---
+++ seprcph/city.py
@@ -0,0 +1,17 @@
+__author__ = 'Flinn'
+"""
+This module contains all classes relating to the trains.
+
+NAME
+    city
+
+FILE
+    seprcph/city.py
+
+CLASSES
+    City
+"""
+
+
+class City(object):
+    def __init__(self, name):
