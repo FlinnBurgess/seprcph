dbd8b58b1596d3f4ad1afa663b6d061956fc6f6e
FlinnBurgess <flinn.burgess@gmail.com>
2014-11-22 23:06:04 +0000

    Tidied up city a bit

--- seprcph/city.py
+++ seprcph/city.py
@@ -17,18 +17,18 @@ class City(object):
     Class that describes the cities shown on the map
     """
 
-
     def __init__(self, name, position, is_capital):
         """
         Args
-        name: Name of the city.
-        position: Co-ordinates of the city on the map.
-        is_capital: Whether or not the city is the capital of it's respective country.
+            name: Name of the city.
+            position: Co-ordinates of the city on the map.
+            is_capital: Whether or not the city is the capital of it's
+                        respective country.
         """
         self.name = name
-        self.position = ()
+        self.position = position
         self.is_capital = is_capital
 
     def update(self):
-    pass
-    ##I've got literally no idea what should go in here. Sorry.##
+        pass
+        ##I've got literally no idea what should go in here. Sorry.
