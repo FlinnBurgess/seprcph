d1bef4c9482e9ad4c5bf9d3e2ec5604e35202339
FlinnBurgess <flinn.burgess@gmail.com>
2014-11-24 13:10:23 +0000

    Update test_track.py
    created a setup method, added city objects as arguments for t, removed unecessary code

--- tests/test_track.py
+++ tests/test_track.py
@@ -1,34 +1,31 @@
 import unittest
 
-from seprcph import track, player
+from seprcph import track, player, city
 
 class TestUnlockTrack(unittest.TestCase):
+    
+    def setUp(self):
+        p = player.Player(50, 1)
+        c1 = city.City("London", (50, 50), True, None, None)
+        c1 = city.City("Birmingham", (50, 50), False, None, None)
+        t = track.Track(c1, c2, 0, 10)
 
     def test_not_enough_gold_player(self):
-        p = player.Player(10, 1)
-        t = track.Track("x", "y", 0, 15)
+        t = track.Track(c1, c2, 0, 55)
         self.assertRaises(track.NotEnoughGoldError, t.unlock_track, p)
 
     def test_track_already_owned(self):
-        p = player.Player(50, 1)
-        t = track.Track("x", "y", 0, 10)
         t.is_locked = False
         self.assertRaises(track.TrackOwnedError, t.unlock_track, p)
 
     def test_owner_is_changing(self):
-        p = player.Player(50, 1)
-        t = track.Track("x", "y", 0, 10)
         t.unlock_track(p)
         self.assertTrue(t.owner == p)
 
     def test_is_unlocking(self):
-        p = player.Player(50, 1)
-        t = track.Track("x", "y", 0, 10)
         t.unlock_track(p)
         self.assertTrue(t.is_locked == False)
 
     def test_gold_updating(self):
-        p = player.Player(50, 1)
-        t = track.Track("x", "y", 0, 10)
         t.unlock_track(p)
         self.assertTrue(p.gold == 40)
