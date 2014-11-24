b568fc3129cc670773f695073fb69e4216ff3b96
FlinnBurgess <flinn.burgess@gmail.com>
2014-11-24 14:48:49 +0000

    Update test_track.py
    This better fukin work. self-dotted EVERYTHING

--- tests/test_track.py
+++ tests/test_track.py
@@ -5,27 +5,27 @@ from seprcph import track, player, city
 class TestUnlockTrack(unittest.TestCase):
     
     def setUp(self):
-        p = player.Player(50, 1, None, None)
-        c1 = city.City("London", (50, 50), True, None, None)
-        c1 = city.City("Birmingham", (50, 50), False, None, None)
-        t = track.Track(c1, c2, 0, 10)
+        self.p = player.Player(50, 1, None, None)
+        self.c1 = city.City("London", (50, 50), True, None, None)
+        self.c2 = city.City("Birmingham", (50, 50), False, None, None)
+        self.t = track.Track(self.c1, self.c2, 0, 10)
 
     def test_not_enough_gold_player(self):
-        t = track.Track(c1, c2, 0, 55)
-        self.assertRaises(track.NotEnoughGoldError, t.unlock_track, p)
+        self.t = track.Track(self.c1, self.c2, 0, 55)
+        self.assertRaises(track.NotEnoughGoldError, self.t.unlock_track, self.p)
 
     def test_track_already_owned(self):
-        t.is_locked = False
-        self.assertRaises(track.TrackOwnedError, t.unlock_track, p)
+        self.t.is_locked = False
+        self.assertRaises(track.TrackOwnedError, self.t.unlock_track, self.p)
 
     def test_owner_is_changing(self):
-        t.unlock_track(p)
-        self.assertTrue(t.owner == p)
+        self.t.unlock_track(self.p)
+        self.assertEqual(self.t.owner, self.p)
 
     def test_is_unlocking(self):
-        t.unlock_track(p)
-        self.assertTrue(t.is_locked == False)
+        self.t.unlock_track(self.p)
+        self.assertEqual(self.t.is_locked, False)
 
     def test_gold_updating(self):
-        t.unlock_track(p)
-        self.assertTrue(p.gold == 40)
+        self.t.unlock_track(self.p)
+        self.assertEqual(self.p.gold, 40)
