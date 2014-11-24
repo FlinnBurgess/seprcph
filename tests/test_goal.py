6ab09a94519fd7486131c79beff59d3ebe40d32c
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-11-24 16:50:55 +0000

    Added a test that fails as a callback requires self as its first argument.

--- tests/test_goal.py
+++ tests/test_goal.py
@@ -1,6 +1,6 @@
 import unittest
 
-from seprcph import goal
+from seprcph import goal, city, player, event
 
 
 class TestCreateGoal(unittest.TestCase):
@@ -13,6 +13,19 @@ class TestCreateGoal(unittest.TestCase):
 
     def test_negative_score_reward(self):
         self.assertRaises(AssertionError, goal.Goal, None, [], 1, 1, -1, None)
+
+class TestCompleteGoal(unittest.TestCase):
+
+    def setUp(self):
+        self.start_city = city.City('London', (100, 100), False, None, None)
+        self.end_city = [city.City('Berlin', (100, 50), False, None, None)]
+        self.player = player.Player(0, 0, None, None)
+        self.goal = goal.Goal(self.start_city, self.end_city, 4, 100, 100, self.player)
+
+    def test_arrive_at_start_city(self):
+        self.assertFalse(self.goal._start_reached)
+        event.EventManager.notify_listeners(event.Event('train.arrive', city=self.start_city))
+        self.assertTrue(self.goal._start_reached)
 
 
 # TODO: Add tests for completing goals, cities need implementing first though.
