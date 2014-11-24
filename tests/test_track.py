d01915f15bf575e3e727c86186d118bfb5c8b95e
FlinnBurgess <flinn.burgess@gmail.com>
2014-11-24 02:12:53 +0000

    Actually fixed unit test

--- tests/test_track.py
+++ tests/test_track.py
@@ -7,13 +7,13 @@ class TestUnlockTrack(unittest.TestCase):
     def test_not_enough_gold_player(self):
         p = player.Player(10, 1)
         t = track.Track("x", "y", 0, 15)
-        self.assertRaises(track.NotEnoughGoldError, t.unlock_track(p))
+        self.assertRaises(track.NotEnoughGoldError, t.unlock_track, p)
 
     def test_track_already_owned(self):
         p = player.Player(50, 1)
         t = track.Track("x", "y", 0, 10)
         t.is_locked = False
-        self.assertRaises(track.TrackOwnedError, track.Track.unlock_track)
+        self.assertRaises(track.TrackOwnedError, t.unlock_track, p)
 
     def test_owner_is_changing(self):
         p = player.Player(50, 1)
