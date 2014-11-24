728a886525f333f0278c2c261bb3e86778599490
al502 <alc551@york.ac.uk>
2014-11-24 14:09:38 +0000

    Update goal.py
    fixed typos

--- seprcph/goal.py
+++ seprcph/goal.py
@@ -2,7 +2,7 @@ from seprcph import event
 
 
 class Goal(object):
-    def __init__(self, start_city, end_cities, turns, gold_reward,points_reward, player, desc=""):
+    def __init__(self, start_city, end_cities, turns, gold_reward, points_reward, player, desc=""):
         """
         Initialise a goal.
 
