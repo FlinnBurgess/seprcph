68337ec4f08e240e08dbf07b0b631bfeb4468dd3
pmb516 <pmb516@york.ac.uk>
2014-11-25 16:01:40 +0000

    Fixed player.py (again)

--- seprcph/player.py
+++ seprcph/player.py
@@ -10,6 +10,7 @@ class Player(object):
             score: The player's current score
             deck: The player's deck
             hand: The cards currently available for the player to use
+        
         """
 
         self.gold = gold
