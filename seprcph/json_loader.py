504443b298e6b11864504a9332f314d81878d384
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-11-25 18:36:30 +0000

    Added docstrings and unit tests.

--- seprcph/json_loader.py
+++ seprcph/json_loader.py
@@ -1,5 +1,16 @@
 import json
 
+
 def objs_from_json_file(file_path, cls):
+    """
+    Create a list of objects that are defined within a JSON file.
+
+    The json file needs to contain a list of the objects, with parameters that
+    match up to those defined in cls.
+
+    Args:
+        file_path: The location of the JSON file to be read.
+        cls: The Class that the object will be created from.
+    """
     with open(file_path, 'r') as json_file:
         return json.load(json_file, object_hook=lambda x: cls(**x))
---
+++ tests/test_json_loader.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+import errno
+import os
+
+from seprcph import json_loader
+
+PATH = 'test.json'
+
+class Foo(object):
+    def __init__(self, string, integer):
+        self.string = string
+        self.integer = integer
+
+class TestJsonFiles(unittest.TestCase):
+
+    def test_no_file(self):
+        self.assertRaises(IOError, json_loader.objs_from_json_file, '', Foo)
+
+    def test_invalid_file(self):
+        self.assertRaises(IOError, json_loader.objs_from_json_file, '/foo/', Foo)
+
+    def test_empty_file(self):
+        with open(PATH, 'w') as f:
+            pass
+        self.assertRaises(ValueError, json_loader.objs_from_json_file,
+                            PATH, Foo)
+
+class TestJsonContents(unittest.TestCase):
+
+    def setUp(self):
+        json.dump([{'string': 'test', 'integer': 1}], open(PATH, 'w'))
+
+    def tearDown(self):
+        try:
+            os.remove(PATH)
+        except OSError as err:
+            if err.errno != errno.ENOENT:
+                raise
+
+    def test_load_object(self):
+        self.assertIsInstance(json_loader.objs_from_json_file(PATH, Foo)[0], Foo)
+
+    def test_load_object_int(self):
+        self.assertIsInstance(json_loader.objs_from_json_file(PATH, Foo)[0].integer, int)
+
+    def test_load_object_str(self):
+        self.assertIsInstance(json_loader.objs_from_json_file(PATH, Foo)[0].string, unicode)
+
+    def test_load_multpile_objs(self):
+        json.dump([{'string': 'test', 'integer': 1},
+                        {'string': 'test2', 'integer': 2}], open(PATH, 'w'))
+        self.assertEquals(len(json_loader.objs_from_json_file(PATH, Foo)), 2)
+
