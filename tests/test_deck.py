e055bf4104f0b6aa73f9c86b962021a1b5b5715e
FlinnBurgess <flinn.burgess@gmail.com>
2014-12-02 09:32:24 +0000

    Added unit tests for the pop, add_to_discard and restart methods

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,3 +1,22 @@
 import unittest, random
 
-from seprcph.deck import Deck, Hand
+from seprcph.deck import Deck
+from seprcph.card import Card
+
+class TestDeckMethods(unittest.TestCase):
+    def setUp(self):
+        self.test_card = Card("test", 1, None, None, None)
+        self.deck = Deck(None, [self.test_card], None)
+
+    def test_deck_pop(self):
+        self.assertEqual(self.deck.pop(), self.test_card)
+
+    def test_add_to_discard(self):
+        self.deck.add_to_discard(self.test_card)
+        self.assertEqual(self.deck.discard.pop(), self.test_card)
+
+    def test_restart(self):
+        self.deck.cards = []
+        self.deck.discard = [self.test_card]
+        self.deck.restart()
+        self.assertEqual(self.deck.discard, [])
