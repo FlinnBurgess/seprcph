6937559f2c4f7df464250e5da59e1e065f12c1ee
flinnburgess <flinn.burgess@gmail.com>
2014-12-13 17:37:43 +0000

    created ui_element superclass

---
+++ seprcph/ui_element.py
@@ -0,0 +1,35 @@
+"""
+This is the superclass for all of out UI elements.
+
+Name:
+    ui_element
+
+File:
+    seprcph/ui_element.py
+
+Classes:
+    UIElement
+"""
+
+class UIElement(object):
+    """
+    Superclass for the UI elements
+    """
+
+    def __init__(self, size, position):
+        """
+        Args:
+            size: a tuple containing the height and width of the UI element
+            position: a tuple containing the coordinates of the UI element
+        """
+        self.size = size
+        self.pos = position
+
+    def __repr__(self):
+        return "<size: %s, position: %s>" % (self.size, self.pos)
+
+    def draw(self):
+        """
+        Tells the game how to draw the UI element
+        """
+    pass
