3bb0f9170ed111d4afda8241cfce65077f8ea036
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-12-16 11:52:12 +0000

    Removed TrackMatrix and added it functionaility into map.

--- seprcph/map.py
+++ seprcph/map.py
@@ -1,9 +1,8 @@
 """ For big-daddy home-slice
 """
 import os.path
-from config import Config
-from json_loader import create_cities, create_tracks
-from track_matrix import TrackMatrix
+from seprcph.config import Config
+from seprcph.json_loader import create_cities, create_tracks
 
 
 class Map(object):
@@ -18,5 +17,47 @@ class Map(object):
                                             "cities.json"))
         self._tracks = create_tracks(os.path.join(Config.general['data_dir'],
                                             "tracks.json"), self._cities)
-        self._track_matrix = TrackMatrix(self._cities, self._tracks)
+
+        # Create a dictionary representation of an enum, mapping a city object
+        # to a unique integer
+        self._cities = {k: v for v, k in enumerate(self._cities)}
+
+        # Create an empty n*n matrix where n is the number of cities
+        self._matrix = [[None] * len(self._cities)] * len(self._cities)
+
+        for track in self._tracks:
+            self.add_track(track)
+
         self.image = image
+
+    def fetch_indices(self, city_pair):
+        """
+            Args:
+                city_pair: A 2-element tuple containing city objects
+
+            Returns:
+                A 2 element tuple containing indices for the pair of cities.
+        """
+        return (self._cities[city_pair[0]],
+                self._cities[city_pair[1]])
+
+    def fetch_track(self, city_pair):
+        """
+            Args:
+                city_pair: A 2-element tuple containing city objects
+
+            Returns:
+                Either the track object corresponding to the pair of cities
+                passed to the method, or None if no such connection exists.
+        """
+        indices = self.fetch_indices(city_pair)
+        return self._matrix[indices[0]][indices[1]]
+
+    def add_track(self, track):
+        """
+            Args:
+                track: The track object to be added.
+        """
+        i = self.fetch_indices(track.cities_connected)
+
+        self._matrix[i[0]][i[1]] = self._matrix[i[1]][i[0]] = track
