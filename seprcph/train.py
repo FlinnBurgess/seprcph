16da3f1d24f741572d658b61c98efd54c6539f4c
flinnburgess <flinn.burgess@gmail.com>
2014-12-16 01:52:04 +0000

    Implemented a Train class - more work needs to be done on the apply_effects method

---
+++ seprcph/train.py
@@ -0,0 +1,45 @@
+"""
+This module contains all classes relating to the trains.
+
+Name:
+    train
+
+Files:
+    seprcph/train.py
+
+Classes:
+    Train
+"""
+import pygame
+
+
+class Train(pygame.sprite.Sprite):
+    """
+    Class representing train objects in the game
+    """
+
+    def __init__(self, buffs, debuffs, speed, capacity):
+        """
+        Args:
+            buffs: list of buffs currently affecting the train
+            debuffs: list of debuffs currently affecting the train
+            speed: the speed of the train
+            capacity: the capacity of the train
+        """
+
+        super(Train, self).__init__()
+        self.buffs = buffs
+        self.debuffs = debuffs
+        self.speed = speed
+        self.capacity = capacity
+
+    def update(self):
+        pass
+
+    ## apply_effects NEEDS REWORKING - this is a placeholder and does not
+    ## fit with the way the cards and decks currently work.
+    def apply_effects(self):
+        for effect in buffs:
+            effect.apply()
+        for effect in debuffs:
+            effect.apply()
