3a2a09f7caa8ad5ada915a436ad619026f154d8f
EthanLaBonche <ejvu500@york.ac.uk>
2014-12-16 16:19:14 +0000

    Seperated the rect tests from the city tests

--- tests/test_city.py
+++ tests/test_city.py
@@ -27,5 +27,7 @@ class TestCityRect (unittest.TestCase):
     def setup(self):
         self.image = pygame.Surface((10, 10))
         self.c = City("London", (50, 50), True, self.image)
+
+    def test_rect_calc(self):
         self.assertEqual(self.c.pos[0], self.c.rect.centerx)
         self.assertEqual(self.c.pos[1], self.c.rect.centery)
