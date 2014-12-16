7f5044527ed435039e1edd65a5ac2b23999fa006
flinnburgess <flinn.burgess@gmail.com>
2014-12-16 00:40:04 +0000

    Fixed some inheritance issues (I think) and added a Label class

--- seprcph/ui_element.py
+++ seprcph/ui_element.py
@@ -23,7 +23,7 @@ File:
     seprcph/ui_element.py
 
 Classes:
-    Element
+    Element, Clickable
 """
 
 class Element(pygame.sprite.Sprite):
@@ -56,14 +56,36 @@ class Clickable(Element):
     A UI element which is clickable by the user
     """
 
-    def __init__(self, callback):
+    def __init__(self, size, position, callback):
         """
         Args:
+            size: a tuple containing the height and width of the UI element
+            position: a tuple containing the coordinates of the UI element
             callback: the function to be called when the element is clicked
         """
-        super(Clickable, self).__init__()
+        super(Clickable, self).__init__(size, position)
         self.callback = callback
 
     def __repr__(self):
         return "<size: %s, position: %s, callback function: %s>" \
                % (self.size, self.pos, self.callback)
+
+
+class Label(Element):
+    """
+    A label UI element, simply contains text to display
+    """
+
+    def __init__(self, size, position,  text):
+        """
+        Args:
+            size: a tuple containing the height and width of the UI element
+            position: a tuple containing the coordinates of the UI element
+            text: text to be displayed in the label
+        """
+        super(Label, self).__init__(size, position)
+        self.text = text
+
+    def __repr__(self):
+        return "<size: %s, position: %s, text: %s>" \
+               % (self.size, self.pos, self.text)
