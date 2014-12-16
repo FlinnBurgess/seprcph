1280c65f2d4316110a26fd15f41a37853d1d3709
flinnburgess <flinn.burgess@gmail.com>
2014-12-16 00:43:50 +0000

    Added a Container class

--- seprcph/ui_element.py
+++ seprcph/ui_element.py
@@ -89,3 +89,23 @@ class Label(Element):
     def __repr__(self):
         return "<size: %s, position: %s, text: %s>" \
                % (self.size, self.pos, self.text)
+
+
+class Container(Element):
+    """
+    A container class which will contain other UI elements
+    """
+
+    def __init__(self, size, position, elements):
+        """
+        Args:
+            size: a tuple containing the height and width of the UI element
+            position: a tuple containing the coordinates of the UI element
+            elements: a list of UI elements contained within the container
+        """
+        super(Container, self).__init__(size, position)
+        self.elements = elements
+
+    def __repr__(self):
+        return "<size: %s, position: %s, elements: %s>" \
+               % (self.size, self.pos, self.elements)
