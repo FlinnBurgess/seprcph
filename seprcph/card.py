21fe2a8341421e37f63ebe76d5d368052d7591ca
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-12-16 13:27:47 +0000

    Fixed bias not being taken into account and renamed create_cards.

--- seprcph/card.py
+++ seprcph/card.py
@@ -1,5 +1,6 @@
 """
-This module contains all classes relating to cards.
+This module contains all classes relating to cards, as well as the factory
+for creating cards
 
 Name:
     card
@@ -9,13 +10,13 @@ File:
 
 Classes:
     Card
+    CardFactory
 """
 import os.path
 import random
 from seprcph.config import Config
 from seprcph.event import Event, EventManager
 from seprcph.json_loader import create_cards
-
 
 
 class Card(object):
@@ -48,6 +49,10 @@ class Card(object):
                                             effect=self.effect))
 
 class CardFactory(object):
+    """
+    Load cards from a json file and create the cards that will be entered
+    into a deck.
+    """
 
     class UnknownBiasError(Exception):
         """
@@ -55,32 +60,47 @@ class CardFactory(object):
         """
         pass
 
-    BIASES = {'aggressive': [10, 15, 15, 10],
-            'defensive': [15, 10, 10, 15]}
+    BIASES = {'aggressive': [0.25, 0.35, 0.20, 0.20],
+            'defensive': [0.35, 0.25, 0.2, 0.2]}
 
     def __init__(self):
+        """
+        Load cards from JSON and them sort them by their type.
+        """
         cards = create_cards(os.path.join(Config.general['data_dir'], 'cards.json'))
         self.buffs = [x for x in cards if x.type is 'Buff']
         self.debuffs = [x for x in cards if x.type is 'Debuff']
         self.traps = [x for x in cards if x.type is 'Traps']
         self.events = [x for x in cards if x.type is 'Events']
 
-    def create_cards(self, count, bias):
+    def build_cards(self, count, bias):
+        """
+        Create count number of cards.
+
+        Args:
+            count: The amount of cards to create.
+            bias: The ratio of each kind of card to use.
+        """
         if bias not in self.BIASES:
             raise self.UnknownBiasError("Bias %s is unknown" % bias)
 
         cards = []
         # Buffs
-        for _ in self.BIASES[bias][0]:
+        for _ in int(self.BIASES[bias][0] * count):
             cards.append(random.choice(self.buffs))
         # Debuffs
-        for _ in self.BIASES[bias][1]:
+        for _ in int(self.BIASES[bias][1] * count):
             cards.append(random.choice(self.debuffs))
         # Traps
-        for _ in self.BIASES[bias][2]:
+        for _ in int(self.BIASES[bias][2] * count):
             cards.append(random.choice(self.traps))
         # Events
-        for _ in self.BIASES[bias][3]:
+        for _ in int(self.BIASES[bias][3] * count):
             cards.append(random.choice(self.events))
+
+        if len(cards) < count:
+            for _ in xrange(count - len(cards)):
+                cards.append(random.choice(self.buffs + self.debuffs
+                                            + self.traps + self.events))
 
         return cards
