3e33752b34c90be835ecfec37fd159c6349e9aae
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-12-18 19:40:27 +0000

    Added the Window class and did a little UI tweaking.

--- seprcph/ui_element.py
+++ seprcph/ui_element.py
@@ -47,6 +47,17 @@ class Element(pygame.sprite.Sprite):
         """
         pass
 
+    def resize(self, w_ratio, h_ratio):
+        """
+        Called when the window is resized.
+
+        Args:
+            w_ratio: The ratio by which the width has changed.
+            h_ratio: The ratio by which the height has changed.
+        """
+        self.size[0] *= w_ratio
+        self.size[1] *= h_ratio
+
 
 class Clickable(Element):
     """
@@ -115,3 +126,49 @@ class Container(Element):
                 elem.click(event)
             except AttributeError:
                 pass
+
+    def draw(self, surface):
+        for elem in self.elems:
+            elem.draw(surface)
+
+        # TODO: Container needs to render itself.
+
+class Window(Container):
+    """
+    The top level of a UI, it manages everything from click processing to
+    organising redrawing.
+    """
+    def __init__(self, size, pos, elems, surface):
+        """
+        Initialise the entire UI
+
+        Args:
+            size: A tuple representing the size of the Window
+            pos: A tuple representing the position of the top left corner of
+            the window
+            elems: The elements that are to be included in this window
+            surface: A pygame.Surface that shall be rendered to
+        """
+        super(Window, self).__init__(size, pos, elems)
+        self.surface = surface
+
+    def resize(self, size):
+        """
+        Called when the main Pygame screen is resized
+
+        Args:
+            size: The new size of the screen
+        """
+        w_ratio = size[0] / self.size[0]
+        h_ratio = size[1] / self.size[1]
+        for elem in self.elems:
+            elem.resize(w_ratio, h_ratio)
+
+        self.size = size
+
+    def draw(self):
+        """
+        Call all of the element's draw methods
+        """
+        for elem in self.elems:
+            elem.draw(self.surface)
