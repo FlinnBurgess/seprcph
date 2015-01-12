233707945b53e6ce84c9ce9ab3548b5dcea13425
flinnburgess <flinn.burgess@gmail.com>
2015-01-12 13:06:46 +0000

    Changed some cock-ups with Surface

--- seprcph/track.py
+++ seprcph/track.py
@@ -44,16 +44,16 @@ class Track(Renderable, Affectable):
         self.cost = cost
         self.rotation = self._calc_rotation(start_city.pos, end_city.pos)
         self.length = self._calc_length(self.cities[0].pos, self.cities[1].pos)
+        self.image = pygame.Surface((2, self.length))
+        self.image.fill((143, 143, 143))
 
         super(Track, self).__init__(((start_city.pos[0] + end_city.pos[0]) /2,
                                     (start_city.pos[1] + end_city.pos[1]) / 2),
-                                    pygame.Surface((2, self.length)))
+                                    self.image)
         Affectable.__init__(self)
 
         self.is_locked = True
         self.owner = None
-
-        self.fill((143, 143, 143))
 
     def __repr__(self):
         return "<connects: %s, %s, gold-gen: %d, cost: %d>" \
