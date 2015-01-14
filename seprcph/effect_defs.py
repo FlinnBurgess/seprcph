685c828abd916716d6d57d770b7a75e3b7bbb5ed
FlinnBurgess <flinn.burgess@gmail.com>
2015-01-14 23:04:28 +0000

    Added a couple of effects

--- seprcph/effect_defs.py
+++ seprcph/effect_defs.py
@@ -1,6 +1,7 @@
 from seprcph.effect import Effect
 from seprcph.train import Train
 from seprcph.player import Player
+from seprcph.track import Track
 
 def pass_function():
     pass
@@ -19,22 +20,26 @@ def reduce_load(train):
 def do_investor(player):
     player.gold += 5
 
-#def do_broken_track(track):
-#    track.is_broken = True
-#    track.image.fill((0, 0, 0))
+def do_broken_track(track):
+    track.is_broken = True
+    track.image.fill((0, 0, 0))
 
-#def undo_broken_track(track):
-#    track.is_broken = False
-#    if track.owner == player1:
-#        track.image.fill((0, 0, 255))
-#    else:
-#        track.image.fill((255, 0, 0))
+def undo_broken_track(track):
+    track.is_broken = False
+    if track.owner.player_id == 1:
+        track.image.fill((0, 0, 255))
+    else:
+        track.image.fill((255, 0, 0))
 
-#def do_undo_hijack_track(track):
-#    if track.owner == player1:
-#        track.owner = player2
-#    else:
-#        track.owner = player1
+def do_undo_hijack_track(track):
+    if track.owner.player_id == 1:
+        track.owner.player_id = 2
+        track.image.fill((255, 0, 0))
+        # This would technically work but have catastrophic effects with
+        # anything that uses a player's ID, we need an alternative.
+    else:
+        track.owner.player_id = 1
+        track.image.fill((0, 0, 255))
 
 #def do_undo_stop_train(train):
 #    if train.speed > 0:
@@ -47,3 +52,5 @@ eff_faster = Effect("Faster", Train, double_speed, half_speed, 3)
 eff_slower = Effect("Slower", Train, half_speed, double_speed, 3)
 eff_train_robbery = Effect("Robbery", Train, reduce_load, pass_function, 1)
 eff_investor = Effect("Investor", Player, do_investor, pass_function, 1)
+eff_broken_track = Effect("Broken Track", Track, do_broken_track, undo_broken_track, 3)
+eff_hijack_track = Effect("Hijack Track", Track, do_undo_hijack_track, do_undo_hijack_track, 1)
